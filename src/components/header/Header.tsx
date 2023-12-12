a31c787d4f3ed33910afc638bc58fab4f00da909
Alisher Nurlybay <areshil91@gmail.com>
2023-12-13 02:05:44 +0600

    logic has been changed for todosDoneCount show

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,24 @@
 import { useTypedSelector } from "../../hooks/useTypedSelector";
-import { selectTodosDoneCount } from "../../store/todos/todosSlice";
+import {
+  selectTodosDoneCount,
+  selectTodosList,
+} from "../../store/todos/todosSlice";
 import Icons from "../icons/Icons";
 import "./Header.scss";
 
 const Header = () => {
   const todosDoneCount = useTypedSelector(selectTodosDoneCount);
+  const todosList = useTypedSelector(selectTodosList);
 
   return (
     <div className="header">
       <h1>TODO APP</h1>
       <Icons />
-      <div className="header__todos-done-count">
-        Количество выполненных задач: <span>{todosDoneCount}</span>
-      </div>
+      {todosList.length > 0 && (
+        <div className="header__todos-done-count">
+          Количество выполненных задач: <span>{todosDoneCount}</span>
+        </div>
+      )}
     </div>
   );
 };
