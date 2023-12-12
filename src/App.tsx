0f8eaf0141a7d2a25f5318176f5e4cc4c1472f74
Alisher Nurlybay <areshil91@gmail.com>
2023-12-13 02:02:06 +0600

    postTodos funct call has been deleted from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import AddTask from "./components/add-task-form/AddTaskBtn";
 import Header from "./components/header/Header";
 import TodosList from "./components/todos-list/TodosList";
-import { postTodos } from "./rtk-query/rtk-query";
 import "./scss/style.scss";
 
 function App() {
-  postTodos()
-
   return (
     <div className="container">
       <div>
